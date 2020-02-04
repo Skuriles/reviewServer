aba3768c048608d1b9c8979403b7b4ac1bad7525
Bene Caspari <skuriles@gmx.de>
2020-02-04 06:23:18 +0100

    host and admin

--- src/routes/basicRoutes.js
+++ src/routes/basicRoutes.js
@@ -6,7 +6,14 @@ var jwt = require("jsonwebtoken");
 const tokenHash = "hhdaii1123";
 // Set some defaults (required if your JSON file is empty)
 db.defaults({
-    users: [],
+    users: [{
+        name: "admin",
+        pw: "admin",
+        id: 1,
+        host: false,
+        admin: true,
+        drinks: []
+    }],
     drinks: []
 }).write();
 
@@ -31,17 +38,34 @@ module.exports = {
     },
     saveDrink: (req, res) => {
         saveDrink(req, res);
+    },
+    setUserAsHost: (req, res) => {
+        setUserAsHost(req, res);
+    },
+    createDrink: (req, res) => {
+        createDrink(req, res);
+    },
+    checkHost: (req, res) => {
+        checkHost(req, res);
     }
+
 };
 
 function login(req, res) {
     const user = req.body.user;
     const token = jwt.sign({
         name: user.name,
-        pw: user.pw
+        pw: user.pw,
+        id: user.id
     }, tokenHash);
-    authenticate(token, (result) => {
+    // nicht mit token sondern username und pw authentifizieren
+    authenticate(user, (result) => {
         if (result) {
+            const token = jwt.sign({
+                name: result.name,
+                pw: result.pw,
+                id: result.id
+            }, tokenHash);
             res.send({
                 token
             });
@@ -57,11 +81,18 @@ function login(req, res) {
             id: id,
             name: user.name,
             pw: user.pw,
+            host: false,
+            admin: false,
             drinks: []
         };
         db.get("users")
             .push(newUser)
             .write();
+        const token = jwt.sign({
+            name: newUser.name,
+            pw: newUser.pw,
+            id: newUser.id
+        }, tokenHash);
         res.send({
             token
         });
@@ -70,15 +101,26 @@ function login(req, res) {
 }
 
 function checkToken(req, res) {
-    authenticate(req.body.token, (result) => {
+    authenticateToken(req.body.token, (result) => {
         res.send(result);
     });
 }
 
-function authenticate(token, callback) {
+function checkHost(req, res) {
+    verifyTokenHost(req, (result) => {
+        if (result) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    });
+}
+
+function authenticateToken(token, callback) {
     return jwt.verify(token, tokenHash, function (err, decoded) {
         if (err && err.length > 0) {
             callback(false);
+            return;
         }
         const dbUser = db.get("users")
             .find({
@@ -88,63 +130,168 @@ function authenticate(token, callback) {
             .value();
         if (dbUser) {
             callback(true);
+            return;
         }
         callback(false);
     });
-
 }
 
+function authenticate(user, callback) {
+    const dbUser = db.get("users")
+        .find({
+            name: user.name,
+            pw: user.pw
+        })
+        .value();
+    callback(dbUser);
+}
+
+function verifyToken(req, callback) {
+    var token = req.headers.authorization;
+    var decoded = jwt.verify(token, tokenHash);
+    if (decoded) {
+        callback(null, decoded);
+    } else {
+        callback("Token ungültig", null);
+    }
+}
+
+function verifyTokenAdmin(req, callback) {
+    var token = req.headers.authorization;
+    var decoded = jwt.verify(token, tokenHash);
+    if (decoded && decoded.role === "Admin") {
+        callback(null, decoded);
+    } else {
+        callback("Kein Admin", null);
+    }
+}
+
+function verifyTokenHost(req, callback) {
+    var token = req.headers.authorization;
+    var decoded = jwt.verify(token, tokenHash);
+    if (decoded) {
+        const host = db.get("users")
+            .find({
+                id: decoded.id
+            })
+            .get("host")
+            .value();
+        callback(null, host);
+    } else {
+        callback("Keine Rechte", false);
+    }
+}
+
+
 function getUserList(req, res) {
-    var users = db.get("users").value();
-    res.send(users);
+    verifyToken(req, (result) => {
+        if (!result) {
+            return [];
+        }
+        var users = db.get("users").value();
+        res.send(users);
+    })
 }
 
 function getItems(req, res) {
-    var list = [];
-    for (let i = 0; i < 3; i++) {
-        var item = {
-            name: "drink" + i,
-            id: i + 1
-        };
-        list.push(item);
-    }
+    verifyToken(req, (result) => {
+        if (!result) {
+            return [];
+        }
+        var list = [];
+        for (let i = 0; i < 3; i++) {
+            var item = {
+                name: "drink" + i,
+                id: i + 1
+            };
+            list.push(item);
+        }
 
-    res.send(list);
-    return;
+        res.send(list);
+        return;
+    });
 }
 
-function addUser(req, res) {
-    // TODO check if needed
-    var user = req.body.user;
-    var users = db.get("users").value();
-    if (nameAlreadyExist(users, user.name)) {
-        res.send(null);
-        return;
-    }
-    var id = getNextId(users);
-    var user = {
-        id: id,
-        name: user.name,
-        pw: user.pw,
-        drinks: []
-    };
-    // Add a post
-    db.get("users")
-        .push(user)
-        .write();
-    res.send({
-        token
+function createDrink(req, res) {
+    verifyTokeHost(req, (result) => {
+        if (!result) {
+            res.send(false);
+            return;
+        }
+        const drink = req.body.item;
+        const id = getNextDrinkId();
+        drink.id = id;
+        db.get("drink")
+            .push(drink)
+            .write();
+        const users = db.get("users").value();
+        for (const user of users) {
+            db.get("users")
+                .find({
+                    id: user.id
+                })
+                .push(drink);
+        }
+        db.write();
+        res.send(true);
+    });
+}
+
+function setUserAsHost(req, res) {
+    verifyTokenAdmin(req, (result) => {
+        if (!result) {
+            return [];
+        }
+        var userId = req.body.id;
+        var users = db.get("users")
+            .value();
+        let userfound = false;
+        // alten Host zurücksetzen
+        for (const user of users) {
+            if (user.host && user.id != userId) {
+                db.get("users")
+                    .find({
+                        id: user.id
+                    })
+                    .set("host", false)
+                    .value();
+            }
+            if (user.id == userId) {
+                db.get("users")
+                    .find({
+                        id: userId
+                    })
+                    .set("host", true)
+                    .value();
+                userfound = true;
+            }
+        }
+        res.send(userfound);
+        return
     });
 }
 
 function saveDrink(req, res) {
-    var drink = req.body.item;
-    // Add a post
+    verifyToken(req, (result) => {
+        if (!result) {
+            return [];
+        }
+        var drink = req.body.item;
+        // Add a post
 
-    db.get("users[0].drinks")
-        .push(drink)
-        .write();
-    res.send(true);
+        db.get("users")
+            .find({
+                id: result.id
+            })
+            .get("drinks")
+            .find({
+                id: drink.id
+            })
+            .set("drink.value", drink.value)
+            .set("drink.comment", drink.comment)
+            .write();
+        res.send(true);
+    });
 }
 
 function getNextId(users) {
@@ -152,7 +299,7 @@ function getNextId(users) {
         return 1;
     }
     users.sort(compareUsers);
-    const lastId = 0;
+    let lastId = 1;
     for (let i = 0; i < users.length; i++) {
         const user = users[i];
         if (i == 0 && user.id != 1) {
@@ -161,6 +308,27 @@ function getNextId(users) {
         if (lastId + 1 != user.id) {
             return lastId + 1;
         }
+        lastId++;
+    }
+}
+
+
+function getNextDrinkId() {
+    const drinks = db.get(drinks).value();
+    if (!drinks || drinks.length == 0) {
+        return 1;
+    }
+    drinks.sort(compareUsers);
+    let lastId = 0;
+    for (let i = 0; i < drinks.length; i++) {
+        const drink = drinks[i];
+        if (i == 0 && drink.id != 1) {
+            return 1;
+        }
+        if (lastId + 1 != drink.id) {
+            return lastId + 1;
+        }
+        lastId++;
     }
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,5 +10,9 @@ router.route('/addUser').post(basicRoutes.addUser);
 router.route('/saveDrink').post(basicRoutes.saveDrink);
 router.route('/login').post(basicRoutes.login);
 router.route('/checkToken').post(basicRoutes.checkToken);
+router.route('/createDrink').post(basicRoutes.createDrink);
+router.route('/setUserAsHost').post(basicRoutes.setUserAsHost);
+router.route('/checkHost').post(basicRoutes.checkHost);
+
 
 module.exports = router;
